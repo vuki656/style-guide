988f4b4686371b649130f312413aa18e43d9307b
Domagoj Vukovic <off.vukovic@gmail.com>
2024-07-21 10:18:37 +0200

    feat: add eslint comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,25 @@
 import { eslintRules } from "./src/eslint/eslint.js"
 import { typescriptEslintRules } from "./src/eslint/typescript-eslint.js"
+import { eslintCommentsRules } from "./src/eslint/eslint-comments.js"
 
 import typescriptEslintPlugin from "typescript-eslint"
+import eslintCommentsPlugin from "@eslint-community/eslint-plugin-eslint-comments"
+
 import typescriptEslintParser from "@typescript-eslint/parser"
+import ESLintPluginESLintCommentsConfigs from "@eslint-community/eslint-plugin-eslint-comments/configs"
 
 export default typescriptEslintPlugin.config({
     rules: {
         ...eslintRules.rules,
         ...typescriptEslintRules.rules,
-    },
+        ...eslintCommentsRules.rules,
+    }, // TODO: should this be here or in plugin file
     plugins: {
-        '@typescript-eslint': typescriptEslintPlugin.plugin,
+        "@typescript-eslint": typescriptEslintPlugin.plugin,
+        "@eslint-community/eslint-comments":
+            ESLintPluginESLintCommentsConfigs.recommended.plugins[
+                "@eslint-community/eslint-comments"
+            ],
     },
     files: ["**/*.js", "**/*.ts"],
     languageOptions: {
---
+++ src/eslint/eslint-comments.js
@@ -0,0 +1,18 @@
+/** @type {import("@types/eslint").Linter.FlatConfig} */
+export const eslintCommentsRules = {
+    rules: {
+        "@eslint-community/eslint-comments/disable-enable-pair": [
+            "error",
+            { allowWholeFile: true },
+        ],
+        "@eslint-community/eslint-comments/no-aggregating-enable": "error",
+        "@eslint-community/eslint-comments/no-duplicate-disable": "error",
+        "@eslint-community/eslint-comments/no-unlimited-disable": "error",
+        "@eslint-community/eslint-comments/no-unused-disable": "error",
+        "@eslint-community/eslint-comments/no-unused-enable": "error",
+        "@eslint-community/eslint-comments/require-description": [
+            "error",
+            { ignore: [] },
+        ],
+    },
+}
