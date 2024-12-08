37ccefd1b9169fa52523f29322544ba0bee42a30
Domagoj Vukovic <off.vukovic@gmail.com>
2024-12-08 10:08:34 +0100

    feat: etc plugin wip

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,14 @@
-// const tsParser = require("@typescript-eslint/parser")
-// const { eslint } = require("./src/eslint/plugins/eslint")
-//
-// /** @type {import("eslint").Linter.Config} */
-// module.exports = [
-//     ...eslint.map((config) => {
-//         return {
-//             ...config,
-//             files: ["**/*.js"],
-//         }
-//     }),
-//     {
-//         files: ["**/*.ts", "**/*.js"],
-//         languageOptions: {
-//             parser: tsParser,
-//             ecmaVersion: 2020,
-//             sourceType: "module",
-//         },
-//     },
-// ]
-
 const tseslint = require("typescript-eslint")
 const { eslint } = require("./src/eslint/plugins/eslint")
 const { eslintComments } = require("./src/eslint/plugins/eslint-comments")
+const { etc } = require("./src/eslint/plugins/etc")
 
-module.exports = tseslint.config(eslint, eslintComments, {
+module.exports = tseslint.config(eslint, eslintComments, etc, {
     languageOptions: {
         parserOptions: {
             projectService: true,
             tsconfigRootDir: __dirname,
+            project: "./tsconfig.json",
         },
     },
 })
--- src/eslint/plugins/etc.js
+++ src/eslint/plugins/etc.js
@@ -1,13 +1,22 @@
-/** @type {import("@types/eslint").ESLint.ConfigData} */
-module.exports = {
-    plugins: ["etc"],
-    rules: {
-        "etc/no-assign-mutated-array": "error",
-        "etc/no-commented-out-code": "error",
-        "etc/no-deprecated": "error",
-        "etc/no-enum": "error",
-        "etc/no-internal": "error",
-        "etc/no-t": "error",
-        "etc/throw-error": "error",
+const etcPlugin = require("eslint-plugin-etc")
+const { fixupPluginRules } = require("@eslint/compat");
+
+/** @type {import("eslint").Linter.Config} */
+const etc = [
+    {
+        plugins: {
+            etc: fixupPluginRules(etcPlugin),
+        },
+        rules: {
+            "etc/no-assign-mutated-array": "error",
+            "etc/no-commented-out-code": "error",
+            "etc/no-deprecated": "error",
+            "etc/no-enum": "error",
+            "etc/no-internal": "error",
+            "etc/no-t": "error",
+            "etc/throw-error": "error",
+        },
     },
-}
+]
+
+module.exports = { etc }
