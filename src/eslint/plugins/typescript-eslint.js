27e580ddd2a35c99bff628b645a07da9856fe566
Domagoj Vukovic <off.vukovic@gmail.com>
2025-01-29 13:58:18 +0100

    feat: remove `@typescript-eslint/ban-types` rule

--- src/eslint/plugins/typescript-eslint.js
+++ src/eslint/plugins/typescript-eslint.js
@@ -7,7 +7,6 @@ module.exports = {
         "@typescript-eslint/await-thenable": "error",
         "@typescript-eslint/ban-ts-comment": "error",
         "@typescript-eslint/ban-tslint-comment": "error",
-        "@typescript-eslint/ban-types": "error",
         "@typescript-eslint/class-literal-property-style": "error",
         "@typescript-eslint/consistent-generic-constructors": "error",
         "@typescript-eslint/consistent-indexed-object-style": "error",
