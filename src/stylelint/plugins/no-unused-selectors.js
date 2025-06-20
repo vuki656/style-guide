f1618c18f704c4b3963abe6be76a7b1305aa4610
Domagoj Vukovic <off.vukovic@gmail.com>
2025-06-20 10:22:15 +0200

    feat: add stylelint-no-unused-selectors plugin

---
+++ src/stylelint/plugins/no-unused-selectors.js
@@ -0,0 +1,10 @@
+/** @type {import("stylelint").Config} */
+const stylelintNoUnusedSelectors = {
+    rules: {
+        "plugin/no-unused-selectors": true,
+    },
+}
+
+module.exports = {
+    stylelintNoUnusedSelectors,
+}
