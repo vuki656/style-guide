6c2edca6a7596e233c32f9c9a783742e07168392
Domagoj Vukovic <off.vukovic@gmail.com>
2026-03-01 15:16:18 +0100

    fix: stylelint unused selectors handle variants file

--- src/stylelint/plugins/no-unused-selectors.js
+++ src/stylelint/plugins/no-unused-selectors.js
@@ -1,7 +1,29 @@
 /** @type {import("stylelint").Config} */
 const plugin = {
     rules: {
-        "plugin/no-unused-selectors": true,
+        "plugin/no-unused-selectors": [
+            true,
+            {
+                resolve: {
+                    documents: [
+                        "{cssDir}/{cssName}.variants.ts",
+                        "{cssDir}/{cssName}.variants.tsx",
+                        "{cssDir}/{cssName}.tsx",
+                        "{cssDir}/{cssName}.jsx",
+                        "{cssDir}/{cssName}.html",
+                        "{cssDir}/{cssName}.htm",
+                        "{cssDir}/{cssDirName}.tsx",
+                        "{cssDir}/{cssDirName}.jsx",
+                        "{cssDir}/{cssDirName}.html",
+                        "{cssDir}/{cssDirName}.htm",
+                        "{cssDir}/index.tsx",
+                        "{cssDir}/index.jsx",
+                        "{cssDir}/index.html",
+                        "{cssDir}/index.htm",
+                    ],
+                },
+            },
+        ],
     },
 }
 
