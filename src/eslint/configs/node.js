eb8533da141cc181bea7d7ed76ed8a060483ea13
Domagoj Vukovic <off.vukovic@gmail.com>
2026-09-13 10:12:44 +0200

    feat(eslint): scripts may print, read env and exit

--- src/eslint/configs/node.js
+++ src/eslint/configs/node.js
@@ -2,7 +2,21 @@ import { NODE_FILES } from "../file-patterns.js"
 import { nodeN } from "../plugins/n.js"
 import { securityNode } from "../plugins/security-node.js"
 
-export const nodeConfig = [nodeN, securityNode]
+const SCRIPT_FILES = ["scripts/**", "**/scripts/**"]
+
+export const nodeConfig = [
+    nodeN,
+    securityNode,
+    {
+        files: SCRIPT_FILES,
+        rules: {
+            "n/no-process-env": "off",
+            "n/no-process-exit": "off",
+            "no-console": "off",
+            "unicorn/no-process-exit": "off",
+        },
+    },
+]
 
 /**
  * Node.js ESLint configuration
